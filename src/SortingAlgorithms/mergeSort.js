cd8a02cc71dfe0e8491fe09e5c46c133ccec6e32
Rudra Sikri <rudrasikri12@gmail.com>
2022-02-13 20:31:24 +0530

    formatted

--- src/SortingAlgorithms/mergeSort.js
+++ src/SortingAlgorithms/mergeSort.js
@@ -1,65 +1,71 @@
 export const getMergeSortAnimations = (array) => {
   const animations = [];
-  if(array.length<=1)
-    return animations;
+  if (array.length <= 1) return animations;
 
   const auxiliaryArray = array.slice();
-  mergeSortHelper(array, 0, array.length-1, auxiliaryArray, animations);
+  mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
   return animations;
-}
+};
 
-function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations){
-  if(startIdx===endIdx)
-    return;
+function mergeSortHelper(
+  mainArray,
+  startIdx,
+  endIdx,
+  auxiliaryArray,
+  animations
+) {
+  if (startIdx === endIdx) return;
 
-  const middleIdx = Math.floor((startIdx + endIdx)/2);
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
   mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
-  mergeSortHelper(auxiliaryArray, middleIdx+1, endIdx, mainArray, animations);
+  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
   merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
 }
 
-function merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations){
-
+function merge(
+  mainArray,
+  startIdx,
+  middleIdx,
+  endIdx,
+  auxiliaryArray,
+  animations
+) {
   let k = startIdx;
   let i = startIdx;
-  let j = middleIdx+1;
+  let j = middleIdx + 1;
 
-  while(i<=middleIdx && j<=endIdx){
+  while (i <= middleIdx && j <= endIdx) {
     //Comparing the values and changing their color
     animations.push([i, j]);
-    
+
     //Reverting back their color
     animations.push([i, j]);
-    if(auxiliaryArray[i] <= auxiliaryArray[j]){
+    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
       animations.push([k, auxiliaryArray[i]]);
       animations.push([k]);
       mainArray[k++] = auxiliaryArray[i++];
-    }
-    else{
+    } else {
       animations.push([k, auxiliaryArray[j]]);
       animations.push([k]);
       mainArray[k++] = auxiliaryArray[j++];
     }
   }
 
-  while(i<=middleIdx){
+  while (i <= middleIdx) {
     animations.push([i, i]);
     animations.push([i, i]);
-    
+
     animations.push([k, auxiliaryArray[i]]);
     animations.push([k]);
     mainArray[k++] = auxiliaryArray[i++];
-    
   }
 
-  while(j<=endIdx){
+  while (j <= endIdx) {
     animations.push([j, j]);
     animations.push([j, j]);
-    
+
     animations.push([k, auxiliaryArray[j]]);
     animations.push([k]);
     mainArray[k++] = auxiliaryArray[j++];
-
   }
 }
-
